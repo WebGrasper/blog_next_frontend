a3575f4eb3764c8654c1f3249fdd7697c95ede1b
WebGrasper <mohdamaan570@gmail.com>
2024-02-19 14:05:09 +0000

    Revert "add google verification id"
    This reverts commit 61fa320070e54d645922f9d4da579fef112bc8d9.

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,19 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
-import Head from "next/head";
 
-import Router from "next/router";
-import NProgress from "nprogress"; //nprogress module
-import "../styles/nprogress.css"; //styles of nprogress
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import '../styles/nprogress.css'; //styles of nprogress
 
-//Route Events.
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
+//Route Events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function App({ Component, pageProps }) {
   return (
     <div>
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="google-site-verification=EeFZuVQzdFS4XDIVNCtJiEW_04egb0d8jV0q-6QE3sg"
-        />
-      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
