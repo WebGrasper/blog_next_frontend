69e56af319e6847e1112f60c09afc2649dcd6ed7
WebGrasper <mohdamaan570@gmail.com>
2024-02-25 11:20:29 +0000

    Added new style

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -35,6 +35,27 @@ function Navbar() {
   //search bar functionality(Ended)
 
   const [isAtPortfolio, setAtPortfolio] = useState(false);
+  useEffect(()=>{
+    window.addEventListener('scroll', () => {
+      if(window.scrollY){
+        setAtPortfolio(true);
+      } else {
+        setAtPortfolio(false);
+      }
+    });
+  })
+
+  const [isOffset, setOffset] = useState(false);
+  useEffect(()=>{
+    console.log(window.innerHeight);
+    window.addEventListener('scroll', () => {
+      if(window.scrollY >= (window.innerHeight - 80)){
+        setOffset(true);
+      } else {
+        setOffset(false);
+      }
+    });
+  })
 
   const pathname = usePathname();
 
@@ -47,34 +68,21 @@ function Navbar() {
     }
   },[pathname]);
   
-styles
   return (
     <div
-      className={`${!isAtPortfolio ? styles.navbarSupremeContainer : styles.nonSticky }`}
+      className={`${styles.navbarSupremeContainer} ${isOffset ? styles.activeOffset : ""}`}
     >
       <div>
         <nav className={`${styles.navbar}`}>
           <div className={`${styles.container1}`}>
             <Link href={"/"}>
               <div className={styles.container1H1}>
-                <img src="/logo.png" alt="stashify logo" />
+                <img src="https://ik.imagekit.io/94nzrpaat/images/gold-logo-with-title-wg_853558-2748-N6dN8fcsA-transformed_1%20(1).png?updatedAt=1708801310085" alt="logo" />
                 <span>Web<span>Grasper</span></span>
               </div>
             </Link>
           </div>
           <div className={`${styles.container2}`}>
-            <form className={`${styles.container2Span}`} method="GET">
-              <input
-                type="search"
-                className={styles.container2SearchBox}
-                id="container2SearchBox"
-                placeholder="Insights, reviews & more..."
-                onChange={(e) => {
-                  handleSearchBlogs(e.target.value || null);
-                }}
-              />
-            </form>
-            <div className={`${styles.searchCheckBoxContainer}`}>
               <input
                 type="checkbox"
                 className={styles.searchCheckBox}
@@ -82,51 +90,50 @@ styles
               />
               <img
                 className={styles.searchButton}
-                src="/searchIcon.png"
+                src={isOffset ? "/searchButtonBlack.svg" : "/searchButtonWhite.svg"}
                 alt="search icon"
               />
               <img
                 className={styles.searchCloseButton}
-                src="/closeButton.png"
+                src={isOffset ? "/closeButtonBlack.svg" : "/closeButtonWhite.svg"}
                 alt="close button"
               />
-            </div>
           </div>
           <div className={`${styles.containerButton}`}>
             <input type="checkbox" className={styles.checkBox} checked={isMenuOpen} onChange={() => setMenuOpen(!isMenuOpen)}/>
             <img
               className={styles.closeButton}
-              src="/closeButton.png"
+              src="/closeButtonBlack.svg"
               alt="close button"
             />
             <img
               className={styles.menuButton}
-              src="/menuButton.png"
+              src={isOffset ? "/menuButtonBlack.svg" : "/menuButtonWhite.svg"}
               alt="menu button"
             />
             <div className={styles.container3Navbar}>
               <Link href="/gadgets-insights" passHref onClick={handleLinkClick}>
-                <span className={styles.link}>Gadgets insights</span>
+                <span className={`${styles.link} ${isOffset ? styles.linkActiveOffset : ""}`}>Gadgets insights</span>
               </Link>
               <Link href="/stack-craft" passHref onClick={handleLinkClick}>
-                <span className={styles.link}>Coding tutorials</span>
+                <span className={`${styles.link} ${isOffset ? styles.linkActiveOffset : ""}`}>Coding tutorials</span>
               </Link>
               <Link href="/portfolio" passHref onClick={handleLinkClick}>
-                <span className={styles.link}>Portfolio</span>
+                <span className={`${styles.link} ${isOffset ? styles.linkActiveOffset : ""}`}>Portfolio</span>
               </Link>
             </div>
           </div>
         </nav>
       </div>
       <form
-        className={`${styles.buttomSearchBarContainer} ${
+        className={`${styles.bottomSearchBarContainer} ${
           isSeachChechBoxChecked ? styles.show : ""
         }`}
         method="GET"
       >
         <input
           type="search"
-          className={styles.buttomSearchBar}
+          className={`${styles.bottomSearchBar} ${isOffset ? styles.activeBottomSearchBar : ""}`}
           id="container2SearchBox"
           placeholder="Ideas, topics & more..."
           onChange={(e) => {
--- pages/index.js
+++ pages/index.js
@@ -7,101 +7,123 @@ import SideBar from "@/components/sidebar";
 export const getServerSideProps = async (context) => {
   let data = null;
   try {
-    let response = await fetch("https://blog-zo8s.vercel.app/app/v2/getArticles", {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    let response = await fetch(
+      "https://blog-zo8s.vercel.app/app/v2/getArticles",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
     data = await response.json();
-
-
   } catch (error) {
     console.error("Error fetching data:", error);
   }
-  let {success, article} = data;
+  let { success, article } = data;
   return {
     props: {
       article,
     },
   };
-}
+};
 
-
-export default function Main({article}) {
-
-
+export default function Main({ article }) {
   return (
     <div>
       <Head>
         <title>WebGrasper: Discover insights, tips, gadgets – your hub!</title>
-        <meta name="description" content="Explore trending tech insights, programming tips, and top gadgets. Stay informed on comparisons, details, and discover the latest in technology." />
+        <meta
+          name="description"
+          content="Explore trending tech insights, programming tips, and top gadgets. Stay informed on comparisons, details, and discover the latest in technology."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <meta property="og:title" content="WebGrasper: Discover insights, tips, gadgets – your hub!" />
-        <meta property="og:description" content="Explore trending tech insights, programming tips, and top gadgets. Stay informed on comparisons, details, and discover the latest in technology." />
+        <meta
+          property="og:title"
+          content="WebGrasper: Discover insights, tips, gadgets – your hub!"
+        />
+        <meta
+          property="og:description"
+          content="Explore trending tech insights, programming tips, and top gadgets. Stay informed on comparisons, details, and discover the latest in technology."
+        />
         <link rel="canonical" href="https://webgrasper.vercel.app/" />
 
-        <meta property="og:image" content="https://webgrasper.vercel.app/logo.png" />
+        <meta
+          property="og:image"
+          content="https://webgrasper.vercel.app/logo.png"
+        />
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       <section className={styles.heroContainer}>
         <picture>
-          <Image src='https://ik.imagekit.io/94nzrpaat/images/hero_image_1.jpg?updatedAt=1708714167492' alt='hero image' className={styles.heroImage} width={100} height={100} unoptimized/>
+          <Image
+            src="https://ik.imagekit.io/94nzrpaat/images/hero-large.jpg?updatedAt=1708846125096"
+            alt="hero image"
+            className={styles.heroImage}
+            width={100}
+            height={100}
+            unoptimized
+          />
         </picture>
-      </section>
-      <div className={styles.homePageSupremeContainer}>
-        <div className={styles.homePageMainContainer}>
-          <div className={styles.dailyArticlesMainContainer}>
-            <h1>Daily Picks</h1>
-            {article &&
-              article.map((article, index) => (
-                <Link
-                  href="/article/[title]"
-                  as={`/article/${encodeURIComponent(article.title.replace(/\s+/g, '-'))}`}
-                  className={styles.dailyArticlesContainer}
-                  key={index}
-                >
-                  <div className={styles.dailyArticleImageContainer}>
-                    <Image
-                      className={styles.dailyArticleImage}
-                      src={article.articleImage[0]}
-                      width={160}
-                      height={160}
-                      alt={article.title}
-                    />
-                  </div>
-                  <div className={styles.dailyArticleTitleContainer}>
-                    <h2 className={styles.dailyArticleTitle}>{article.title}</h2>
-                    <p className={styles.dailyArticleDescription}>{article?.description.slice(0, 250)}&hellip;</p>
-                    <div className={styles.dailyArticleDateContainer}>
-                      <h3 className={styles.dailyArticleDate}>
-                        Updated &#8226;{" "}
-                        {article.createdAt
-                          .slice(0, 10)
-                          .split("-")
-                          .reverse()
-                          .join("-")}
-                      </h3>
-                      <h4 className={styles.dailyCategory}>
-                        Category &#8226; {article.category}
-                      </h4>
-                    </div>
-                  </div>
-                </Link>
-              ))}
-          </div>
-          <div className={styles.homePageSideBarContainer}>
-            <SideBar />
-          </div>
+        <div className={styles.heroContent}>
+          <h1>
+            <span>Stay</span>
+            <span>Curious.</span>
+          </h1>
         </div>
-      </div>
+      </section>
+      <section className={styles.homePageSupremeContainer}>
+        <div className={styles.dailyArticleHeadingContainer}>
+          <h1>Daily Picks</h1>
+        </div>
+        <div className={styles.dailyArticlesMainContainer}>
+          {article &&
+            article.map((article, index) => (
+              <Link
+                href="/article/[title]"
+                as={`/article/${encodeURIComponent(
+                  article.title.replace(/\s+/g, "-")
+                )}`}
+                className={styles.dailyArticlesContainer}
+                key={index}
+              >
+                <div className={styles.dailyArticleImageContainer}>
+                  <img
+                    className={styles.dailyArticleImage}
+                    src={article.articleImage[0]}
+                    alt={article.title}
+                  />
+                </div>
+                <div className={styles.dailyArticleTitleContainer}>
+                  <h2 className={styles.dailyArticleTitle}>{article.title}</h2>
+                  <p className={styles.dailyArticleDescription}>
+                    {article?.description.slice(0, 100)}&hellip;
+                  </p>
+                  <div className={styles.dailyArticleDateContainer}>
+                    <h3 className={styles.dailyArticleDate}>
+                      {article.createdAt
+                        .slice(0, 10)
+                        .split("-")
+                        .reverse()
+                        .join("-")}
+                    </h3>
+                    <h4 className={styles.dailyCategory}>{article.category}</h4>
+                  </div>
+                </div>
+              </Link>
+            ))}
+        </div>
+        {/* <div className={styles.homePageSideBarContainer}>
+            <SideBar />
+          </div> */}
+      </section>
     </div>
   );
 }
